c5d1601a93825fdfb378d8681e1c9da35f06fa31
Ahmet Tekçe <ahmettekce@msn.com>
2021-01-23 13:14:27 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,TValue>
+    {
+        private T[] _key;
+        private TValue[] _value;
+        private int _count=0;
+
+        public MyDictionary()
+        {
+            _key = new T[_count];
+            _value = new TValue[_count];
+        }
+        public void Add(T anahtar, TValue deger)
+        {
+            _count++;
+
+            T[] tempKey = _key;
+            TValue[] tempValue = _value;
+
+            _key = new T[_count + 1];
+            _value = new TValue[_count + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+                _value[i] = tempValue[i];
+            }
+
+            _key[_count - 1] = anahtar;
+            _value[_count - 1] = deger;
+        }
+
+        public T[] Anahtar
+        {
+            get { return _key; }
+        }
+
+        public TValue[] Deger
+        {
+            get { return _value; }
+        }
+        public int Count
+        {
+            get { return _count; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<>
+            MyDictionary<int, string> iller = new MyDictionary<int, string>();
+            iller.Add(1,"Adana");
+            iller.Add(6,"Ankara");
+            iller.Add(7,"Antalya");
+
+            for (int i = 0; i < iller.Count; i++)
+            {
+                Console.WriteLine($"Plaka Kodu: {iller.Anahtar[i]} - İl: {iller.Deger[i]}");
+            }
+
+            Console.WriteLine("Toplam eleman: "+iller.Count);
+        }
+    }
+}
